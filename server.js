ffb24949b5fa6702d213cb6e3bc3afb65f415140
Sofia <asofiae96@gmail.com>
2022-08-04 22:48:32 -0500

    working

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 // Initialize express app
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 // Setup data parsing
 app.use(express.urlencoded({ extended: true }));
